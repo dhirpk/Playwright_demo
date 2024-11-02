d5ca0c5c24b9c0bdbccdcd68351820e93dbbf60a
mymac <dhirpk@hotmail.com>
2024-11-02 18:12:42 +0100

    Commit

---
+++ Playwright_demo/NUnitPlaywright.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace Playwright_demo;
+
+public class NUnitPlaywright : PageTest
+{
+    [SetUp]
+    public async Task SetupAsync()
+    {
+        await Page.GotoAsync(url: "https://allegro.pl");
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        await Page.ClickAsync(selector: "text=Moje Allegro");
+        await Page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "demo2.jpg"
+        });
+
+
+
+    }
+}
---
+++ Playwright_demo/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Playwright_demo;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
